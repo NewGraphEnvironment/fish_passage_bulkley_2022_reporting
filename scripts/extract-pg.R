3833119b66cc725dc757895e1933aaf304aff026
almac2022 <al@newgraphenvironment.com>
2023-01-06 16:23:44 -0800

    build with changes to background and waterfall

--- scripts/extract-pg.R
+++ scripts/extract-pg.R
@@ -4,12 +4,23 @@ source('R/packages.R')
 
 conn <- DBI::dbConnect(
   RPostgres::Postgres(),
-  dbname = "postgis",
+  dbname = "bcfishpass",
   host = "localhost",
   port = "5432",
   user = "postgres",
   password = "postgres"
 )
+
+dbGetQuery(conn,
+           "select t.table_schema,
+           t.table_name
+           from information_schema.tables t
+           inner join information_schema.columns c on c.table_name = t.table_name
+           and c.table_schema = t.table_schema
+           where c.column_name = 'transport_line_surface_code'
+           and t.table_schema not in ('information_schema', 'pg_catalog')
+           and t.table_type = 'BASE TABLE'
+           order by t.table_schema;")
 
 
 ##get our study area
