18f34593ea7db33fdcaa090a46043893179a5201
newgraph-lschick <lucy@newgraphenvironment.com>
2024-04-07 10:42:06 -0700

    changed to devtools::session_info(), closes #147

--- scripts/packages.R
+++ scripts/packages.R
@@ -1,6 +1,6 @@
-# install.packages('pacman')
+# install.packages('pak')
 
-package_list <- c(
+pkgs_cran <- c(
   #'plotKML', #this takes forever to load so going to leave it out for now
   'raster', #load this dog before dplyr yo
   'tidyverse',
@@ -32,14 +32,11 @@ package_list <- c(
   'english',
   'leaflet.extras',
   'ggdark',
-  'fwapgr',
   'citr',
   'pdftools',
-  'pacman',
   'chron',
   'leafpop',
   'exifr',
-  'fpr',  #personal package available on github - see below
   'readODS'
   # rgl,
   # geojsonsf,
@@ -47,10 +44,28 @@ package_list <- c(
   # gert  ##to track git moves
 )
 
-lapply(package_list,
+pkgs_gh <- c(
+  "newgraphenvironment/fpr",
+  "poissonconsulting/fwapgr",
+  'poissonconsulting/poisspatial',
+  "crsh/citr",
+  "poissonconsulting/fishbc"
+)
+
+pkgs_all <- c(pkgs_cran,
+              pkgs_gh)
+
+# install or upgrade all the packages with pak
+lapply(pkgs_all,
+       pak::pkg_install, ask = FALSE)
+
+# load all the packages
+pkgs_ld <- c(pkgs_cran,
+             basename(pkgs_gh))
+
+lapply(pkgs_ld,
        require,
        character.only = TRUE)
-
 
 # for a fresh install of R
 # lapply(package_list,
@@ -59,15 +74,6 @@ lapply(package_list,
 
 # we need the development version of pagedown as of 20200303 https://github.com/rstudio/pagedown/issues/265
 # remotes::install_github('rstudio/pagedown')
-
-
-pacman::p_load_gh("poissonconsulting/fwapgr",
-                  'poissonconsulting/poisspatial',
-                  "crsh/citr",
-                  'rstudio/pagedown',
-                  "poissonconsulting/fishbc",
-                  "newgraphenvironment/fpr")
-# "poissonconsulting/subfoldr2")
 
 
 # custom package
