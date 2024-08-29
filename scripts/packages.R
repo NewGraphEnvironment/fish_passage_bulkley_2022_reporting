1ce7f5c9dd2082fc3338b504b43b9b9d54743460
almac2022 <al@newgraphenvironment.com>
2024-08-29 07:59:23 -0700

    use param for pkg update and call fpr directly to scab bc of https://github.com/NewGraphEnvironment/fpr/issues/94

--- scripts/packages.R
+++ scripts/packages.R
@@ -38,18 +38,23 @@ pkgs_cran <- c(
 
 
 pkgs_gh <- c(
-  "newgraphenvironment/fpr",
+  #hased out fpr due to https://github.com/NewGraphEnvironment/fpr/issues/94
+  # "NewGraphEnvironment/fpr",
   "poissonconsulting/fwapgr",
   'poissonconsulting/poisspatial',
-  "haozhu233/kableExtra"
+  # watch for issues in the future with this particular pin to deal with black captions
+  # https://github.com/NewGraphEnvironment/mybookdown-template/issues/50
+  "haozhu233/kableExtra@a9c509a"
 )
 
 pkgs_all <- c(pkgs_cran,
               pkgs_gh)
 
 # install or upgrade all the packages with pak
-lapply(pkgs_all,
-       pak::pkg_install, ask = FALSE)
+# install or upgrade all the packages with pak
+if(params$update_packages){
+  lapply(pkgs_all, pak::pkg_install, ask = FALSE)
+}
 
 # load all the packages
 pkgs_ld <- c(pkgs_cran,
@@ -59,7 +64,6 @@ lapply(pkgs_ld,
        require,
        character.only = TRUE)
 
-# for a fresh install of R
-# lapply(package_list,
-#        install.packages,
-#        character.only = TRUE)
+# load fpr due to https://github.com/NewGraphEnvironment/fpr/issues/94
+library(fpr)
+
